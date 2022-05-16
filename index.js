91742c22bfe0e21554298efb5f251645349a4d64
Greensky-gs <72513011+Greensky-gs@users.noreply.github.com>
2022-05-16 07:40:40 +0200

    index.js

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ const clean = (message) => {
   if (!message.member.permissions.has("MANAGE_MESSAGES")) return message.channel.send(`:x: | You **must** have the \`manage messages\` permission!`);
   if (!message.guild.me.permissions.has("MANAGE_MESSAGES")) return message.channel.send(`:x: | I **must** have the \`manage messages\` permission!`);
   
-  message.channel.messages.cache.filter((msg) => !msg.pinned && msg.deletable && !msg.deleted).forEach((msg) => {
-    msg.delete().catch(() => {});
-  });
+  const messages = message.channel.messages.cache.filter(x => x.deletable && !x.pinned);
+  message.channel.bulkDelete(messages).catch(() => {});
+  
   message.channel.send(`:white_check_mark: | Start to supress messages`);
 };
 
